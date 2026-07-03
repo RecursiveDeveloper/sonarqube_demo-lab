9c87e2e325a90ac9599c6c8b29fc2439d1a78582
jhjortiz <jhjortiz@bancolombia.com.co>
2026-07-03 10:14:00 -0500

    User data script was optimized and shortened

--- iac/scripts/user_data.sh
+++ iac/scripts/user_data.sh
@@ -2,48 +2,27 @@
 
 SONARQ_USER="sonarqube"
 SONARQ_USER_PASS="Pragm@11"
-SONARQ_USER_PATH="/home/$SONARQ_USER"
-BINARY_PATH="https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.4.0.54424.zip"
-ZIP_FILE_NAME="sonarqube_install.zip"
-UNZIPPED_FILE_NAME="sonarqube_install"
+SONARQ_USER_PATH="/home/${SONARQ_USER}"
+BINARY_PATH="https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.9.8.100196.zip"
+FILE_NAME="sonarqube_install"
 
-echo -e "\n---------------------------------------------"
-echo -e "Installing dependencies"
-echo -e "---------------------------------------------\n"
-sudo apt update     -y
-sudo apt install    -y openjdk-11-jre-headless
-sudo apt install    -y unzip
-sudo apt install    -y net-tools
+echo -e "\n----------Installing dependencies----------\n"
+apt update  -y
+apt install -y openjdk-17-jre-headless
+apt install -y unzip
+apt install -y net-tools
 
-cd /
+echo -e "\n----------Manage user----------\n"
+useradd -m -s /bin/bash "${SONARQ_USER}"
+echo "${SONARQ_USER}:${SONARQ_USER_PASS}" | chpasswd
+usermod -aG sudo "${SONARQ_USER}"
+chown -R "${SONARQ_USER}:${SONARQ_USER}" "${SONARQ_USER_PATH}"
+chmod 711 "${SONARQ_USER_PATH}"
 
-echo -e "\n---------------------------------------------"
-echo -e "Add user"
-echo -e "---------------------------------------------\n"
-sudo useradd -m -s /bin/bash "$SONARQ_USER"
-echo "$SONARQ_USER:Pragm@11" | sudo chpasswd
-
-echo -e "\n---------------------------------------------"
-echo -e "Change permissions"
-echo -e "---------------------------------------------\n"
-sudo usermod -aG sudo "$SONARQ_USER"
-sudo chown -R "$SONARQ_USER:$SONARQ_USER" "$SONARQ_USER_PATH"
-sudo chmod 711 "$SONARQ_USER_PATH"
-
-echo -e "\n---------------------------------------------"
-echo -e "Move location"
-echo -e "---------------------------------------------\n"
-cd $SONARQ_USER_PATH
-echo $SONARQ_USER_PASS | sudo -S -u $SONARQ_USER bash <<EOF
-
-echo -e "\n---------------------------------------------"
-echo -e "Get installer"
-echo -e "---------------------------------------------\n"
-wget $BINARY_PATH -O ./$ZIP_FILE_NAME
-unzip ./$ZIP_FILE_NAME -d ./$UNZIPPED_FILE_NAME
-
-echo -e "\n---------------------------------------------"
-echo -e "Executing SonarQube"
-echo -e "---------------------------------------------\n"
-./$UNZIPPED_FILE_NAME/sonarqube*/bin/linux*/sonar.sh console
+echo -e "\n----------Install and execute Sonarqube----------\n"
+cd ${SONARQ_USER_PATH}
+echo ${SONARQ_USER_PASS} | su -s /bin/bash ${SONARQ_USER} <<EOF
+wget ${BINARY_PATH} -O ./${FILE_NAME}.zip
+unzip ./${FILE_NAME}.zip -d ./${FILE_NAME}
+./${FILE_NAME}/sonarqube*/bin/linux*/sonar.sh start
 EOF
